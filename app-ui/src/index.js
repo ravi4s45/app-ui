e14609ef06c628fb8804f1c27cff86c0d62def78
HarshitaPatil21 <66163787+ravi4s45@users.noreply.github.com>
2021-09-15 18:02:32 +0530

    Part 3 - Using React Diagrams

--- app-ui/src/index.js
+++ app-ui/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BasicConnection from './BasicConnection';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BasicConnection />
   </React.StrictMode>,
   document.getElementById('root')
 );
